36496ea45d272c2d4bba4ef8915c0eb7a71b75e3
Festo Wampamba <festotechug@gmail.com>
2026-01-11 14:05:14 +0300

    Fixed also a typo for orgaization table, updated states to district to match the Ugandan standards, and also added contract on jobListing Types for NGO's, also updated Joblisting statuses by replacing delisted with closed

--- src/drizzle/schema/organization.ts
+++ src/drizzle/schema/organization.ts
@@ -1,7 +1,7 @@
 import { pgTable, varchar } from "drizzle-orm/pg-core";
 import { createdAt, updatedAt } from "../schemaHelpers";
 
-export const OrganiztionTable = pgTable("organization", {
+export const OrganizationTable = pgTable("organization", {
   id: varchar().primaryKey(),
   name: varchar().notNull(),
   imageUrl: varchar(),
